bd01025909e72e341ea219b611c93d497641efc8
Richard Armijos <richardmijo@hotmail.com>
2023-11-30 10:19:39 -0500

    Proyecto inicial

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println("Hello World!")
+
+    /**
+     * String
+     * Int
+     * Double
+     * Float
+     * Boolean
+     */
+
+    // variable mutables
+    var miVariable : String = "sss"
+    println(miVariable)
+    // cambiar valor
+
+    // solo lectura
+    val soloLectura = 5.2
+}
